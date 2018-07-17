3d8d3f529bfd8dec992f9ff8bb89dc44145c846d
Andrew <andrew.merison@gmail.com>
2018-07-17 16:38:25 -0700

    Cleaned up the initial commit.

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -4,17 +4,17 @@ const router = Router();
 
 // Index Route
 router.get('/', (req, res) => {
-	res.render('index', { cache: true });
+	res.render('pages/index', { cache: true });
 });
 
 // 404 Handler
 router.get('/404', (req, res) => {
-	res.status(404).render('404', { cache: true });
+	res.status(404).render('pages/404', { cache: true });
 });
 
 // Backup route in case no others are defined
 router.get('/:route', (req, res) => {
-	res.redirect(301, '/404');
+	res.redirect(301, 'pages/404');
 });
 
 export const mainRouter = router;

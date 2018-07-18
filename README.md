b04169e9c5dd8ba9ac41bf32dc43667aee69e671
Andrew <andrew.merison@gmail.com>
2018-07-17 17:02:26 -0700

    Added basic README

---
+++ README.md
@@ -0,0 +1,15 @@
+# Node CMS
+
+This will have actual content later on. For now, here's the different scripts that are useful when working with this project:
+
+### Install Dependencies
+`npm install` or `yarn`
+
+### Build Typescript Files
+`npm run build` or `yarn build`
+
+### Watch Typescript Files
+`npm run watch` or `yarn watch`
+
+### Nodemon Auto-Reload
+`npm run dev` or `yarn dev`
--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -4,17 +4,17 @@ const router = Router();
 
 // Index Route
 router.get('/', (req, res) => {
-	res.render('pages/index', { cache: true });
+	res.render('index', { cache: false, title: 'Home' });
 });
 
 // 404 Handler
 router.get('/404', (req, res) => {
-	res.status(404).render('pages/404', { cache: true });
+	res.status(404).render('404', { cache: false, title: '404' });
 });
 
 // Backup route in case no others are defined
 router.get('/:route', (req, res) => {
-	res.redirect(301, 'pages/404');
+	res.redirect(301, '404');
 });
 
 export const mainRouter = router;
